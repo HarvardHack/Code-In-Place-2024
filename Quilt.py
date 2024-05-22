30c1bdd8cb7a1937d0272eb99901e36fbb0df209
HarvardHack <rvanegap@aol.com>
2024-05-21 22:14:56 -0500

    Update Quilt.py

--- Quilt.py
+++ Quilt.py
@@ -10,13 +10,20 @@ def main():
     # draw the first row of patches
     draw_square_patch(canvas, 0, 0)
     draw_circle_patch(canvas, PATCH_SIZE, 0)
-    draw_square_patch(canvas, PATCH_SIZE*2, 0)
-    draw_circle_patch(canvas, PATCH_SIZE*3, 0)
-    
+    draw_square_patch(canvas, PATCH_SIZE * 2, 0)
+    draw_circle_patch(canvas, PATCH_SIZE * 3, 0)
+    # TODO: your code here
+    # draw the second row of patches
+    draw_circle_patch(canvas, 0, PATCH_SIZE)
+    draw_square_patch(canvas, PATCH_SIZE, PATCH_SIZE)
+    draw_circle_patch(canvas, PATCH_SIZE * 2, PATCH_SIZE)
+    draw_square_patch(canvas, PATCH_SIZE * 3, PATCH_SIZE)
     
 def draw_circle_patch(canvas, start_x, start_y):
-    
-    pass
+    # draw a salmon circle that fills the patch at the provided location
+    end_x = start_x + PATCH_SIZE
+    end_y = start_y + PATCH_SIZE
+    canvas.create_oval(start_x, start_y, end_x, end_y, 'salmon')
 
 def draw_square_patch(canvas, start_x, start_y):
     # draws a purple frame at (start_x, start_y)
@@ -26,8 +33,8 @@ def draw_square_patch(canvas, start_x, start_y):
     # first draw a purple square over the entire patch
     canvas.create_rectangle(start_x, start_y, end_x, end_y, 'purple')
     # then draw a smaller white square on top
-    canvas.create_rectangle(start_x+inset, start_y+inset, 
-        end_x-inset, end_y-inset, 'white')
+    canvas.create_rectangle(start_x + inset, start_y + inset, 
+                            end_x - inset, end_y - inset, 'white')
     
 if __name__ == '__main__':
     main()
