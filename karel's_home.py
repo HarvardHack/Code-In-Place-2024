63d008d587a645ce8ee1609d0201b8b029322488
HarvardHack <rvanegap@aol.com>
2024-05-21 20:58:23 -0500

    Create karel's_home.py

---
+++ karel's_home.py
@@ -0,0 +1,55 @@
+from karel.stanfordkarel import *
+
+# File: shelter.py
+# -----------------------------
+# The warmup program defines a "main"
+# function which should make Karel 
+# move to the beeper, pick it up, and
+# return home.
+def main():
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    pick_beeper()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    move()
+    turn_left()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    turn_left()
+    
+    
+    
+# don't edit these next two lines
+# they tell python to run your main function
+if __name__ == '__main__':
+    main()
