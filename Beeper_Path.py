efd3aaeb4e4f1f67e7c52f44b5b3319e31d5d6ce
HarvardHack <rvanegap@aol.com>
2024-05-21 21:02:09 -0500

    Create Beeper_Path.py

---
+++ Beeper_Path.py
@@ -0,0 +1,10 @@
+from karel.stanfordkarel import * 
+
+def main():
+    if beepers_present():
+    # move to empty cell
+            while beepers_present():
+                move()
+
+if __name__ == '__main__':
+    main()
