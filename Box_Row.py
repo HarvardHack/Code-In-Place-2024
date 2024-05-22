9e358df6f1cffbfd4bc3ef232d92405a8ae3a20e
HarvardHack <rvanegap@aol.com>
2024-05-21 22:54:28 -0500

    Update Box_Row.py

--- Box_Row.py
+++ Box_Row.py
@@ -7,15 +7,15 @@ BOX_SIZE = CANVAS_WIDTH / N_BOXES
 
 def main():
     canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
-    for i in range (5):
-        left_x =  i * 80
-        top_y = 120
-        right_x = left_x + BOX_SIZE 
-        bottom_y = top_y + BOX_SIZE 
-        canvas.create_rectangle(left_x,top_y,right_x ,bottom_y ,"white", "black")
+    
+    for i in range(N_BOXES):
+        left_x = i * BOX_SIZE
+        top_y = CANVAS_HEIGHT - BOX_SIZE
+        right_x = left_x + BOX_SIZE
+        bottom_y = CANVAS_HEIGHT
+        canvas.create_rectangle(left_x, top_y, right_x, bottom_y, "white", "black")
 
-
-# There is no need to edit code beyond this point
+    canvas.display()
 
 if __name__ == '__main__':
     main()
