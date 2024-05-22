85fdf6d8c9c8e3577a63cd13d3569b2df70a0f52
HarvardHack <rvanegap@aol.com>
2024-05-21 21:00:33 -0500

    Create 2024_Karel.py

---
+++ 2024_Karel.py
@@ -0,0 +1,18 @@
+from karel.stanfordkarel import *
+
+"""
+When you finish writing this file, Karel should be able to 
+place 20 beepers, then 24 beepers, and end facing East to 
+the right of the 24 beepers.
+"""
+
+def main():
+    for i in range (20):
+      put_beeper()
+    move()
+    for i in range (24):
+       put_beeper()
+    move()   
+
+if __name__ == '__main__':
+    main()
