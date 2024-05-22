8d244ba518cf4db36c46f2504b72577c89b29d6a
HarvardHack <rvanegap@aol.com>
2024-05-21 21:01:17 -0500

    Create Piles.py

---
+++ Piles.py
@@ -0,0 +1,51 @@
+from karel.stanfordkarel import *
+
+# File: piles.py
+# -----------------------------
+# The warmup program defines a "main"
+# function which should make Karel
+# pick up all the beepers in the world.
+def main():
+    move()
+    pick_beeper()
+    pick_beeper()
+    pick_beeper()
+    pick_beeper()
+    pick_beeper()
+    pick_beeper()
+    pick_beeper()
+    pick_beeper()
+    pick_beeper()
+    pick_beeper()
+    move()
+    move()
+    pick_beeper()
+    pick_beeper()
+    pick_beeper()
+    pick_beeper()
+    pick_beeper()
+    pick_beeper()
+    pick_beeper()
+    pick_beeper()
+    pick_beeper()
+    pick_beeper()
+    move()
+    move()
+    pick_beeper()
+    pick_beeper()
+    pick_beeper()
+    pick_beeper()
+    pick_beeper()
+    pick_beeper()
+    pick_beeper()
+    pick_beeper()
+    pick_beeper()
+    pick_beeper()
+    move()
+   
+   
+   
+# don't edit these next two lines
+# they tell python to run your main function
+if __name__ == '__main__':
+    main()
