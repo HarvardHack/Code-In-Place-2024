b0263105ba0995c71c9dea729ee89f9b3e489441
HarvardHack <rvanegap@aol.com>
2024-05-21 21:23:04 -0500

    Create Draw_Flag.py

---
+++ Draw_Flag.py
@@ -0,0 +1,13 @@
+from graphics import Canvas
+import random
+
+CANVAS_WIDTH = 450
+CANVAS_HEIGHT = 300
+
+def main():
+    canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+    # TODO, your code here
+    
+
+if __name__ == '__main__':
+    main()
